8b79466e84c7cbb92890e1a5a466251e898ef991
Farry Argoebie <fargobie@gmail.com>
2016-04-19 07:22:56 +0000

    added routing to admin-index from login page

--- server.js
+++ server.js
@@ -49,14 +49,6 @@ app.get('/register', function(req, res) {
     res.sendFile(__dirname + '/public/register.html');
 });
 
-app.get('/login', function(req, res) {
-    res.sendFile(__dirname + '/public/login.html');
-});
-
-app.get('/loginTest', function(req, res) {
-    res.sendFile(__dirname + '/public/pages-login.html');
-});
-
 app.listen(process.env.PORT, function(err){
     if (err) throw err;
     console.log("Server is running at port: " + process.env.PORT + " and IP: " + process.env.IP );
