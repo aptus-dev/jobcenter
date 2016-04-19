7fc2da846694fdfd3b6e6b162f0197a4c12432a9
Farry Argoebie <fargobie@gmail.com>
2016-04-19 07:29:37 +0000

    added /login route to server.js while route.js is being rebuilt by @audiae

--- server.js
+++ server.js
@@ -49,6 +49,11 @@ app.get('/register', function(req, res) {
     res.sendFile(__dirname + '/public/register.html');
 });
 
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/public/pages-login.html');
+});
+
+
 app.listen(process.env.PORT, function(err){
     if (err) throw err;
     console.log("Server is running at port: " + process.env.PORT + " and IP: " + process.env.IP );
